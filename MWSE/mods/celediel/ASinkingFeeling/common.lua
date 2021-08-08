f0c5bc190ed8c22ca88a9934d6dc1eb309e1e5e9
Lilian Jónsdóttir <lilian.jonsdottir@gmail.com>
2021-08-07 23:06:33 -0700

    oops leftover working title

--- MWSE/mods/celediel/ASinkingFeeling/common.lua
+++ MWSE/mods/celediel/ASinkingFeeling/common.lua
@@ -7,6 +7,6 @@ this.modInfo = "No longer can you swim in heavy plate, now your armour, equipmen
 this.version = "1.0.0"
 this.configString = string.gsub(this.modName, "%s+", "")
 this.modes = {equippedArmour = 0, allEquipment = 1, encumbrancePercentage = 2}
-this.log = function(...) mwse.log("[%s] %s", "Armoured Sinking", string.format(...)) end
+this.log = function(...) mwse.log("[%s] %s", this.modName, string.format(...)) end
 
 return this

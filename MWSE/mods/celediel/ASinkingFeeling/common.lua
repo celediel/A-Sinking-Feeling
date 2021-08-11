83d01ec04a5ab71744ac4dca4945c0490d9d9600
Lilian Jónsdóttir <lilian.jonsdottir@gmail.com>
2021-08-11 09:48:34 -0700

    version bump

--- MWSE/mods/celediel/ASinkingFeeling/common.lua
+++ MWSE/mods/celediel/ASinkingFeeling/common.lua
@@ -5,7 +5,7 @@ this.author = "Celediel"
 this.modInfo = "No longer can you swim in heavy plate; now your armour, equipment, or carried items drag you down while swimming.\n\n" ..
 	"Options exist for formulas based on equipped armour weight class, total equipment weight or encumbrance percentage.\n\n" ..
 	"What is dead may never die."
-this.version = "1.1.0"
+this.version = "1.2.0"
 this.configString = string.gsub(this.modName, "%s+", "")
 this.modes = {
 	equippedArmour = {
